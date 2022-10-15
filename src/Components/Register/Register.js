fdd1701f0724e22bd609e9c802e379d0bf5587c6
MahmodulShahin <mahmodulshahin665@gmail.com>
2022-10-15 14:37:14 +0600

    add form in register and add style

--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -5,7 +5,15 @@ const Register = () => {
     return (
         <div className='register'>
             <h3>Please Register!!</h3>
-
+            <form>
+                <input type="text" placeholder='your name' />
+                <br />
+                <input type="email" placeholder='your email' />
+                <br />
+                <input type="password" name="" id="" placeholder='password' />
+                <br />
+                <button className='submit-btn' type="submit">Login</button>
+            </form>
         </div>
     );
 };
