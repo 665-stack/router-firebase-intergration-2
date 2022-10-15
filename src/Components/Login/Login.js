7a220de537b65775492923bbac43224d327ed9bf
MahmodulShahin <mahmodulshahin665@gmail.com>
2022-10-15 14:25:09 +0600

    add form in login page and add style

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -5,6 +5,22 @@ const Login = () => {
     return (
         <div className='login'>
             <h3>Please Login</h3>
+            <button className='thirdPartyLogin'>Google</button>
+            <button className='thirdPartyLogin'>Facebook</button>
+            <button className='thirdPartyLogin'>Github</button>
+
+            <br />
+
+            <span className='or-btn'>OR</span>
+
+            <form>
+                <input type="email" placeholder='your email' />
+                <br />
+                <input type="password" name="" id="" placeholder='password' />
+                <br />
+                <button className='submit-btn' type="submit">Login</button>
+            </form>
+
         </div>
     );
 };
--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -5,9 +5,7 @@ const Register = () => {
     return (
         <div className='register'>
             <h3>Please Register!!</h3>
-            <button className='thirdPartyLogin'>Google</button>
-            <button className='thirdPartyLogin'>Facebook</button>
-            <button className='thirdPartyLogin'>Github</button>
+
         </div>
     );
 };
