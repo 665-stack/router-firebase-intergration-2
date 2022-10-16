84baa6eee6b18906ca45e10f0399acb148168302
MahmodulShahin <mahmodulshahin665@gmail.com>
2022-10-16 13:01:29 +0600

    show userName in products page

--- src/Components/Products/Products.js
+++ src/Components/Products/Products.js
@@ -1,9 +1,15 @@
+import { getAuth } from 'firebase/auth';
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import app from '../../firebase.init';
 import './Products.css'
+
+const auth = getAuth(app)
 const Products = () => {
+    const [user] = useAuthState(auth)
     return (
         <div className='products'>
-            <h3>Hi bro, you can find our product here. <br /> <span>So you can order them :) </span></h3>
+            <h3>Hi <span className="userNameInProducts">{user ? user.displayName : "ghost"}</span>, you can find our product here. <br /> <span>So you can order them :) </span></h3>
         </div>
     );
 };
