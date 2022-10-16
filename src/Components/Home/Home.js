06f488fddaa3424737f26fc8e103c86dcab1bc7b
MahmodulShahin <mahmodulshahin665@gmail.com>
2022-10-16 12:44:36 +0600

    show userName in home page

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,10 +1,16 @@
+import { getAuth } from 'firebase/auth';
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import app from '../../firebase.init';
 import './Home.css'
 
+const auth = getAuth(app)
 const Home = () => {
+    const [user] = useAuthState(auth)
+
     return (
         <div className='home'>
-            <p>Hey fuckin Ghost! Welcome</p>
+            <p>Hey, fuckin <span className='userNameInHomepage'>{user ? user.displayName : 'Ghost'}!</span> Welcome.</p>
             <h3>Who we are? If you know, you know.</h3>
         </div>
     );
